4655895ee8893076c9fc53d72da7cff49db5a566
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2024-07-04 22:11:03 +0200

    Initial commit. Works with Dallas temperature

---
+++ install-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Arduino IDE's default Sketchbook location
+cd "${HOME}/Documents/Arduino"
+cd libraries
+
+git clone https://github.com/ttlappalainen/NMEA2000.git
+git clone https://github.com/ttlappalainen/CAN_BUS_Shield.git
+git clone https://github.com/ttlappalainen/NMEA2000_mcp.git
